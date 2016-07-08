ee6b53a4dfe6c0c6f7223c530806ff7efa830a7b
leoxalapa <leoveracruz@hotmail.com>
2016-07-08 11:02:31 -0500

    hola  bienvenidos2

---
+++ 7ProyectoConjuntos/src/pkg7proyectoconjuntos/Main.java
@@ -0,0 +1,15 @@
+
+
+package pkg7proyectoconjuntos;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Operaciones op = new Operaciones();
+        
+        op.ejecutar();
+    }
+    
+}
